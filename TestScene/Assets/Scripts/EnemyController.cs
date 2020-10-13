d86deef0d22dd294fefa98a5f30c9aaf5637605d
Lm42010 <56361448+Lm42010@users.noreply.github.com>
2020-10-13 17:52:45 -0400

    TestScene
    a very simple scene to test the player and enemy movements

---
+++ TestScene/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // "Public" Variables
+    [SerializeField] private float speed;
+    [SerializeField] private float timeValue;
+    // [SerializeField] private Vector2 pointA;
+    // [SerializeField] private Vector2 pointB;
+
+    // Private Variables
+    private Rigidbody2D rBody;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = timeValue;
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Physics (Update is called once per frame)
+    private void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time > 0)
+        {
+            rBody.velocity = new Vector2(speed * -1, 0.0f);
+        }
+        else if (time > (timeValue * -1))
+        {
+            rBody.velocity = new Vector2(speed, 0.0f);
+        }
+
+        // OLD Back and forth movement
+        // float time = Mathf.PingPong(Time.time * speed, 1);
+        // transform.position = Vector2.Lerp(pointA, pointB, time);
+    }
+}
---
+++ TestScene/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // "Public" Variables
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 500.0f;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+
+    // Private Variables
+    private Rigidbody2D rBody;
+    private bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Physics (Update is called once per frame)
+    private void FixedUpdate()
+    {
+        float hori = Input.GetAxis("Horizontal");
+        isGrounded = GroundCheck();
+
+        // Jump
+        if(isGrounded && (Input.GetAxis("Jump") > 0))
+        {
+            rBody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
+
+        rBody.velocity = new Vector2(hori * speed, rBody.velocity.y);
+    }
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+}
