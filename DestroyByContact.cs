54d172b06438f45fb62011d77541fea32beaf576
Lm42010 <56361448+Lm42010@users.noreply.github.com>
2020-10-16 19:45:23 -0400

    DestroyByContact
    The code for the missile. Now, it only destroys enemies and no other object.

---
+++ DestroyByContact.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    // "Public" Variables
+    // [SerializeField] private GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Detect when another object collides with this object
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Create and position the explosion
+        // Instantiate(explosion, transform.position, transform.rotation);
+
+        // Delete the enemy object and no other object
+        // Delete this object
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+        else if (!(other.gameObject.CompareTag("Enemy")))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
